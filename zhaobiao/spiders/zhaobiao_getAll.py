291f17f1250a7fd8e9788df30ab772c8a8170451
vill <544912473@qq.com>
2018-11-27 23:46:03 +0800

    it's a maybe a completed get_all spider without any spider

--- zhaobiao/spiders/zhaobiao_getAll.py
+++ zhaobiao/spiders/zhaobiao_getAll.py
@@ -6,7 +6,7 @@ import datetime
 import traceback
 
 "this is a dictionary of chinese province"
-d = {
+provinces = {
     110000: "beijing",
     120000: "tianjing",
     310000: "shanghai",
@@ -40,18 +40,55 @@ d = {
     460000: "hainan"
 }
 
+provinces_index = [
+    110000,
+    120000,
+    310000,
+    500000,
+    130000,
+    410000,
+    530000,
+    210000,
+    230000,
+    430000,
+    340000,
+    370000,
+    650000,
+    320000,
+    330000,
+    360000,
+    420000,
+    450000,
+    620000,
+    140000,
+    150000,
+    620000,
+    220000,
+    350000,
+    520000,
+    440000,
+    630000,
+    540000,
+    510000,
+    640000,
+    460000
+]
+
 
 class ZhaobiaoSpider(scrapy.Spider):
     name = "zhaobiao_getAll"
 
     def start_requests(self):
+        province_index = 0
         url = 'http://www.bidchance.com/' \
               'freesearch.do?&filetype=&channel=gonggao&currentpage=1' \
               '&searchtype=sj&queryword=&displayStyle=title&pstate=&' \
-              'field=all&leftday=&province=&bidfile=&project=&' \
+              'field=all&leftday=&province=%d&bidfile=&project=&' \
               'heshi=&recommend=&field=all&jing=&starttime=&endtime=&' \
-              'attachment='
-        yield scrapy.Request(url=url, callback=self.parse)
+              'attachment=' % provinces_index[province_index]
+        yield scrapy.Request(url=url, callback=self.parse,
+                             meta={'province_index': province_index,
+                                   'currentPage': 1})
 
     def parse(self, response):
         conn = mysql.connector.connect(host='localhost', port='3306', user='vill', password='hao5jx', database='spider')
@@ -74,25 +111,45 @@ class ZhaobiaoSpider(scrapy.Spider):
                   "('%s','%s','%s','%s','%s')" % (tablename, title, url, location, channel, pubdate)
             try:
                 cursor.execute(sql)
-            except :
+            except:
                 mlog = open("error_vill_zhaobiao.log", "a")
                 dt = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-                mlog.write('[%s]\n' % dt)
+                mlog.write('>>>>>[%s]\n' % dt)
                 traceback.print_exc(file=mlog)
                 mlog.write('\n')
+                mlog.write("the url of the worn page is \n%s\n" % url)
+                mlog.close()
             conn.commit()
         next_page = response.xpath(
             '//div[@class = "fy l"]/div[@class= "fynr"]/div[@id = "nextpage2"]/a/@href').extract_first()
-        if next_page is not None:
+        currentPage = int(response.meta['currentPage'])
+        if (currentPage == 500) or (next_page is None):
+            province_index = int(response.meta["province_index"])
             mlog = open("vill.log", "a")
             dt = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-            mlog.write('[%s]_%s\n' % (str(dt), str(next_page)))
+            mlog.write('[%s]province %s is over\n' % (str(dt), provinces[provinces_index[province_index]]))
+            mlog.close()
+            province_index += 1
+            url = 'http://www.bidchance.com/' \
+                  'freesearch.do?&filetype=&channel=gonggao&currentpage=1' \
+                  '&searchtype=sj&queryword=&displayStyle=title&pstate=&' \
+                  'field=all&leftday=&province=%d&bidfile=&project=&' \
+                  'heshi=&recommend=&field=all&jing=&starttime=&endtime=&' \
+                  'attachment=' % provinces_index[province_index]
+            yield scrapy.Request(url=url, callback=self.parse,
+                                 meta={'province_index': province_index,
+                                       'currentPage': 1})
+        elif next_page is not None:
+            mlog = open("vill.log", "a")
+            dt = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+            mlog.write('[%s]currentPage is %s\n url of next page is %s\n' % (str(dt), currentPage, str(next_page)))
+            mlog.close()
             s = random.randint(10, 20)
-            print(s)
+            print('sleep:%s' % s)
+            province_index = int(response.meta["province_index"])
+            print('province index is %d' % province_index)
             time.sleep(s)
             next_page = response.urljoin(next_page)
-            yield scrapy.Request(url=next_page, callback=self.parse)
-        else:
-            mlog = open("vill.log", "a")
-            dt = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-            mlog.write('[%s]_province_ _is_over\n' % (str(dt), str(next_page)))
+            yield scrapy.Request(url=next_page, callback=self.parse,
+                                 meta={'province_index': response.meta['province_index'],
+                                       'currentPage': (int(response.meta['currentPage']+1))})
